85bb376994aa4eb28ee8850789661ef78dff10f5
harshita vishnoi <harshitavishnoi33@gmail.com>
2025-05-13 23:56:19 +0530

    initial commit

---
+++ trading_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = 'Stock App',
+    page_icon = "chart_with_downward_trend:",
+    layout = "wide"
+)
+
+st.title('Stock prediction :bar_chart:')
+
+st.header('GOAL : To predict the stock market trends and future outcomes and train different models')
+
+st.image('app.png')
+
