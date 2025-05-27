fdefefa8411f63cea793a077460affa49a7144b5
harshita vishnoi <harshitavishnoi33@gmail.com>
2025-05-27 06:50:35 +0530

    data analysis

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # stock_analysis_app
+
+# dependencies to install
+
+python
+pip install streamlit
+pip install yfinance
+pip install ta
+pip install pandas
+pip install plotly
+pip install numpy
+
+
+# start the sever 
+streamlit run trading_app.py
--- pages/stock_data_analysis.py
+++ pages/stock_data_analysis.py
@@ -1,2 +1,44 @@
 import pandas as pd 
 import numpy as np
+import streamlit as st
+import yfinance as yf
+import plotly.graph_objects as go
+import datetime 
+import ta
+
+st.set_page_config(
+    page_title = 'Stock Analysis',
+    page_icon = "page with curls",
+    layout = "wide"
+)
+
+st.title('Analysis')
+
+col1, col2, col3 = st.columns(3)
+
+today = datetime.date.today()
+
+with col1:
+    ticker = st.text_input("Stock Ticker", "TSLA")
+with col2:
+    start_date = st.date_input("Choose a Start Date", datetime.date(today.year-1, today.month, today.day))
+with col3:
+    end_date = st.date_input("Choose a End Date", datetime.date(today.year, today.month, today.day))
+
+
+st.subheader(ticker)
+
+stock = yf.Ticker(ticker)
+
+# st.write(stock.info)
+
+st.write(stock.info['longBusinessSummary'])
+st.write("**Sector:**",stock.info['sector'])
+st.write("**Full Time Employees:**",stock.info['fullTimeEmployees'])
+st.write("**Website:**",stock.info['website'])
+
+col1, col2 = st,columns()
+
+with col1:
+    df = pd.DataFrame(index = ['Market Cap', 'Beta', 'Eps', 'PE Ratio'])
+    df[''] = [stock.info["marketCap"], stock.info['beta'], stock.info['trailingEps'], stock.info['trailingPE']]
